e620ddbed58d3a1cac3bd0d8486a0fe118c55ac3
Default User <mhraju069@gmail.com>
2026-02-26 01:40:45 +0600

    Update: serializers

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -2,9 +2,27 @@ from rest_framework import serializers
 from .models import *
 
 class BookingSerializer(serializers.ModelSerializer):
+    guests_count = serializers.IntegerField()
+
     class Meta:
         model = Booking
         fields = '__all__'
+        read_only_fields = ['user']
+
+    def validate(self, attrs):
+        if attrs['time_slot'].available_capacity() < attrs['guests_count']:
+            raise serializers.ValidationError(f"Not enough capacity.Available capacity {attrs['time_slot'].available_capacity()}")
+        return attrs
+
+    def create(self, validated_data):
+        print("validated_data 😊😊😊",validated_data)
+        # validated_data['service'] = Service.objects.get(title=validated_data['service'])
+        booking = Booking.objects.create(**validated_data)
+        slot = validated_data['time_slot']
+        slot.booked_capacity += validated_data['guests_count']
+        slot.save()
+        
+        return booking
 
 
 class SlotSerializer(serializers.ModelSerializer):
