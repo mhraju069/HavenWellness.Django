1208c6d6f34257879c8b7c21c4880791f723189e
Default User <mhraju069@gmail.com>
2026-02-25 23:52:52 +0600

    add: setup admin.py

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,9 @@
+from .models import *
 from django.contrib import admin
-
+from unfold.admin import ModelAdmin
 # Register your models here.
+
+admin.site.register(Slot, ModelAdmin)
+admin.site.register(TimeSlot, ModelAdmin)
+admin.site.register(Booking, ModelAdmin)
+admin.site.register(AccessCode, ModelAdmin)
--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Service, ServiceImage, ServiceFeature
+from unfold.admin import ModelAdmin
 # Register your models here.
+
+admin.site.register(Service, ModelAdmin)
+admin.site.register(ServiceImage, ModelAdmin)
+admin.site.register(ServiceFeature, ModelAdmin)
