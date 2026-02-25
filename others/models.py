8c7b5766209b92140b86a8e2e241537494dc90c3
Default User <mhraju069@gmail.com>
2026-02-26 03:54:02 +0600

    add: FAQ

--- others/models.py
+++ others/models.py
@@ -24,3 +24,42 @@ class SupportMessage(models.Model):
 
     def __str__(self):
         return f"Message {self.id} - {self.sender.username} - {self.type}"
+
+
+class TermsAndConditions(models.Model):
+    effective_date = models.DateField(blank=True, null=True)
+    acceptance_of_terms = models.TextField(blank=True, null=True)
+    user_responsibilities = models.TextField(blank=True, null=True)
+    ticket_purchase = models.TextField(blank=True, null=True)
+    refund_policy = models.TextField(blank=True, null=True)
+    limitation_of_liability = models.TextField(blank=True, null=True)
+    changes_to_terms = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Terms and Conditions"
+
+
+class PrivacyPolicy(models.Model):
+    effective_date = models.DateField(blank=True, null=True)
+    info_we_collect = models.TextField(blank=True, null=True)
+    how_we_use_data = models.TextField(blank=True, null=True)
+    child_safety = models.TextField(blank=True, null=True)
+    user_rights = models.TextField(blank=True, null=True)
+    security = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Privacy Policy"
+
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
