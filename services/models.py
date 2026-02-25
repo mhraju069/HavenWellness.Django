d25c3472d9dd1f3b6d54aad8f269ec787401fa22
Default User <mhraju069@gmail.com>
2026-02-25 23:52:31 +0600

    fix : services models

--- services/models.py
+++ services/models.py
@@ -16,20 +16,19 @@ class Service(models.Model):
         ('partial', 'Partial Payment'),
         ('on_spot', 'On Spot Payment'),
     ]
-    title = models.CharField(max_length=100)
+    title = models.TextField(choices=TYPE_CHOICES, default='private_sauna',unique=True,primary_key=True)
     subtitle = models.CharField(max_length=100, blank=True, null=True)
     description = models.TextField()
     capacity = models.IntegerField(default=1)
     duration = models.IntegerField(default=60)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    type = models.CharField(max_length=20, choices=TYPE_CHOICES, default='private_sauna')
     payment_type = models.CharField(max_length=20, choices=PAYMENT_TYPE_CHOICES, default='full')
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.title} | {self.type}"
+        return f"{self.title} | {self.capacity} | {self.price}"
 
 
 class ServiceImage(models.Model):
@@ -40,7 +39,7 @@ class ServiceImage(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Image for {self.service.title} | {self.service.type}"
+        return f"Image for {self.service.title}"
 
 
 class ServiceFeature(models.Model):
