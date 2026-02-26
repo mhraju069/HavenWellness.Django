0758eead0bb06f584fb3a1e7c605bf10d640e1cb
Default User <mhraju069@gmail.com>
2026-02-27 00:45:28 +0600

    add: ContactInfo

--- others/models.py
+++ others/models.py
@@ -63,3 +63,16 @@ class FAQ(models.Model):
 
     def __str__(self):
         return self.question
+
+
+class ContactInfo(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=255)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
