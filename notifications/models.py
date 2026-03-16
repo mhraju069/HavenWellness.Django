4eb6ff31e542bf2b88054b5eac86a5330dfa9b30
Raju <mhraju069@gmail.com>
2026-03-17 02:34:20 +0600

    add: add send_push_notification

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Device(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    token = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+class Notification(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+import firebase_admin
+from firebase_admin import credentials, messaging
+from django.conf import settings
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Device
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class DeviceRegisterView(APIView):
+    def post(self, request):
+        token = request.data.get('token')
+        if not token:
+            return Response({'error': 'Token not found'}, status=400)
+
+        Device.objects.get_or_create(user=request.user, token=token)
+        return Response({'message': 'Device registered successfully'})
+
+
+def send_bulk_notification(tokens, title, body):
+    message = messaging.MulticastMessage(
+        notification=messaging.Notification(title=title, body=body),
+        tokens=tokens,
+    )
+    response = messaging.send_each_for_multicast(message)
+    print(f"✅ Success: {response.success_count}, ❌ Failed: {response.failure_count}")
+
+
+def send_push_notification(token, title, body, data=None):
+
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=title,
+            body=body,
+        ),
+        token=token,
+        data=data or {},
+    )
+
+    try:
+        response = messaging.send(message)
+        print(f"✅ Notification sent: {response}")
+        return True
+    except Exception as e:
+        print(f"❌ Error: {e}")
+        return False
+
+
+def notify_user(user, title, body):
+    devices = Device.objects.filter(user=user)
+    for device in devices:
+        send_push_notification(
+            token=device.token,
+            title=title,
+            body=body,
+        )
